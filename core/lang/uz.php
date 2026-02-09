ef89aa93548b930dc5c4f463b98f58c4698cdd49
Lesnik82 <alexanderalexandrov2021@mail.ru>
2026-02-10 03:25:00 +1000

    Add files via upload

---
+++ core/lang/uz.php
@@ -0,0 +1,321 @@
+<?
+
+# Пишу скрипты, модули с 0 
+# PHP <=> 7.4, MySQL, MySQL(i), AJAX
+# Author Xaori69otori
+# Telegram @Xaori69otori
+
+# Узбекский язык
+
+$lang = array(
+
+'home'						=>		'Asosiy',
+'auth'						=>		'Kirish',
+'reg'						=>		'Ro\'yxatdan o\'tish',
+'recovery'					=>		'Qayta tiklash',
+'download'					=>		'Yuklab olish',
+'nick'						=>		'Nick',
+'nick-email'				=>		'Nick yoki Elektron pochta',
+'login'						=>		'Kirish',
+'password' 	   				=>		'Parol',
+'come' 	       				=>		'Kirish',
+'no-pass' 	       			=>		'Parolni unutdingizmi',
+'new-account' 	    		=>		'Akkaunt yaratish',
+'yes-rules' 	    		=>		'Qoidalar bilan tanishdim',
+'reg-email' 	    		=>		'Haqiqiy Elektron pochtangizni kiriting, akkauntni faollashtirish uchun unga havola yuboriladi',
+'ruless' 	   				=>		'qoidalari',
+'accept-rules' 	   			=>		'Siz qoidalarni qabul qilishingiz kerak',
+'subs' 	       				=>		'Obuna bo\'lish',
+'follows'     				=>		'Obunachilar',
+'publi'     				=>		'Nashrlar',
+'edit'          			=>		'Tahrirlash',
+'delete'        			=>		'O\'chirish',
+'contacts'      			=>		'Aloqa',
+'feed'          			=>		'Yangiliklar',
+'profile'       			=>		'Profil',
+'notify'        			=>		'Bildirishnoma',
+'direct'        			=>		'Murojaatlar',
+'settings'      			=>		'Sozlamalar',
+'adminka'          			=>		'Adminpanel',
+'exit'          			=>		'Chiqish',
+'p-subs'          			=>		'Obuna bo\'lish',
+'p-unsubs'          		=>		'Obunani bekor qilish',
+'p-nosubs'          		=>		'So\'rovni bekor qilish',
+'edit-photo'    			=>		'Profil rasmini o\'zgartirish',
+'name'          			=>		'Ism',
+'user-name'     			=>		'Foydalanuvchi nomi',
+'site'          			=>		'Sayt',
+'biog'          			=>		'Bioqrafiya',
+'d-edit'        			=>		'Shaxsiy ma\'lumotlarni sozlash',
+'email'         			=>		'Elektron manzil',
+'phone'         			=>		'Telefon raqami',
+'floor'         			=>		'Jins',
+'birthday'      			=>		'Tug\'ilgan kun',
+'main-settings' 			=>		'Asosiy sozlamalar',
+'edit-about'    			=>		'Ixtiyoriy ma\'lumotlarni ko\'rsating, hatto agar akkaunt shirkat, uy janubiyatlari yoki boshqa maqsad uchun foydalanilsa ham. Bu ma\'lumotlar umumiy profilizda ko\'rsatilmaydi',
+'privacy'       			=>		'Maxfiylik',
+'security'      			=>		'Xavfsizlik',
+'account'       			=>		'Akkaunt',
+'dakr-mode'     			=>		'Kecha rejimi',
+'lang'          			=>		'Sayt tili',
+'balance'       			=>		'Balans',
+'set-view-profile'       	=>		'Mening sahifamni ko\'rishi mumkin',
+'set-direct'       			=>		'Menga xabar jo\'natish mumkin',
+'set-view-fav'       		=>		'Saqlanganlarimni ko\'rish mumkin',
+'all-users'       			=>		'Barcha foydalanuvchilar',
+'only-subs'       			=>		'Faqat obunachilar',
+'just-me'       			=>		'Faqat men',
+'set-view-profile-text' 	=>		'Agar akkauntingiz yopiq bo\'lsa, sizning surat va videolaringizni faqat siz tasdiqlovchi odamlar ko\'rishi mumkin. Bu mavjud obunachilaringizni ta\'sir qilmaydi',
+'set-online'       	=>		'O\'nline holat',
+'set-online-show'       	=>		'O\'nline holatni ko\'rsatish',
+'set-online-text'       	=>		'Obunachilaringiz va sizga xabar yuborganlar o\'nline bo\'lishingizni va ya oxirgi marta qachon o\'nline bo\'lganingizni ko\'rishlariga imkon berishni ruxsat beradi. Ushbu sozlamani o\'chirsangiz, siz boshqa foydalanuvchilarning o\'nline holatini ko\'rmaysiz.',
+'set-pass'       			=>		'Eski parol',
+'set-n-pass'       			=>		'Yangi parol',
+'set-n-pass2'       		=>		'Yangi parolni qayta kiriting',
+'set-err-n-pass1'       	=>		'Eski va yangi parolni kiriting',
+'set-err-n-pass2'       	=>		'Yangi parolni noto\'g\'ri tasdiqladingiz',
+'set-err-n-pass3'       	=>		'Eski va yangi parolni kamida 3 ta belgidan iborat kiriting',
+'set-err-n-pass4'       	=>		'Eski parolda kirillcha harf yasak',
+'set-err-n-pass5'       	=>		'Yangi parolda kirillcha harf yasak',
+'set-err-n-pass6'       	=>		'Eski parolni noto\'g\'ri kiriting',
+'set-n-pass-succes'       	=>		'Parol o\'zgartirildi',
+'no-public'       			=>		'Nashr yo\'q',
+'no-notify'       			=>		'Bildirishnoma yo\'q',
+'no-fav'       				=>		'Saqlanganlar yo\'q',
+'no-subs'       			=>		'Obunachilar yo\'q',
+'no-follows'       			=>		'Obunachilar yo\'q',
+'no-request'       			=>		'So\'rovlarm yo\'q',
+'no-direct'       			=>		'Chatlar yo\'q',
+'no-mess'       			=>		'Xabarlar yo\'q',
+'no-like'       			=>		'Yoq',
+'no-reg'					=>		'Ro\'yxatdan o\'tkazish vaqti kelib chiqdi. <br> Keyinroq kirib ko\'ring.',
+'no-users'       			=>		'Foydalanuvchilar yo\'q',
+'no-admins'       			=>		'Adminlar yo\'q',
+'no-attach'       			=>		'Xabar biriktirilmagan',
+'no-info'       			=>		'Aniq ma\'lumot',
+'only-for-subs'       		=>		'Faqat obunachilar uchun',
+'search'         			=>		'Qidiruv',
+'for-you'         			=>		'Siz uchun',
+'accounts'         			=>		'Akkauntlar',
+'places'         			=>		'Joylar',
+'tags'         				=>		'Markazlar',
+'search-no'         		=>		'Hech nima topilmadi',
+'help'         				=>		'Yordam',
+'sogl'         				=>		'Shogirdlar',
+'male'         				=>		'Erkak',
+'female'       				=>		'Ayol',
+'another'       			=>		'Boshqa',
+'no-sex'       				=>		'Ko\'rsatmasdan yaxshi deb o\'ylayman',
+'title'         			=>		'Ism',
+'save'         				=>		'Saqlash',
+'back'         				=>		'Orqaga',
+'text'         				=>		'Matn',
+'photo'         			=>		'Rasm',
+'write'    					=>		'Yozish',
+'mess'    					=>		'Xabar',
+'your-mess'    				=>		'Sizning xabaringiz',
+'new-public'    			=>		'Yangi nashr',
+'new-story'    				=>		'Yangi hikoya',
+'upload'    				=>		'Yuklab olish',
+'publish'    				=>		'Nashr etish',
+'add'    					=>		'Qo\'shish',
+'like'    					=>		'Yoqdi',
+'liked'    					=>		'Sizga yoqdi',
+'more'    					=>		'Ko\'proq',
+'public'    				=>		'Nashr',
+'post'    					=>		'Post',
+'stories'    				=>		'Hikoyalar',
+'light'    					=>		'Yorug\'',
+'dark'    					=>		'Tungi',
+'accept'    				=>		'Qabul qilish',
+'reject'    				=>		'Rad etish',
+'signa'    					=>		'Imzo',
+'add-signa'    				=>		'Imzo qo\'shish',
+'add-place'    				=>		'Joy qo\'shish',
+'example'    				=>		'Namuna',
+'data-saved'    			=>		'Malumotlar saqlandi',
+'clear'    					=>		'Keshni tozalash',
+'all'    					=>		'Barchasi',
+'new'    					=>		'Yangi',
+'enter-nick'				=>		'Nickni kiriting',
+'error-nick'				=>		'Taqiqlangan taxallus',
+'enter-pass'				=>		'Parolni kiriting',
+'enter-email' 	 			=>		'Elektron pochtani kiriting',
+'err-nick-long' 	 		=>		'Nick uzunligi kamida 4 belgi, maksimum 30 belgi bo\'lishi kerak',
+'err-nick-lang' 	 		=>		'Nickda kirillchadan foydalanish taqiqlanadi',
+'err-num' 	 				=>		'Faqat raqamlardan foydalanish taqiqlanadi',
+'err-sym' 	 				=>		'Bir hil belgilardan foydalanish taqiqlanadi',
+'err-pass-long' 	 		=>		'Parol uzunligi kamida 5 belgi bo\'lishi kerak',
+'err-nick-close' 	 		=>		'Bu nom bilan akkaunt allaqachon mavjud',
+'err-email-close' 	 		=>		'Bu Elektron pochta bilan akkaunt allaqachon mavjud',
+'sent-conf' 	 			=>		'Sizning Elektron pochtaga akkauntni tasdiqlash xabari yuborildi',
+'sent-pass' 	 			=>		'Sizning Elektron pochtaga yangi parol yuborildi',
+'account-act' 	 			=>		'Akkaunt faollashtirildi',
+'err-email' 	 			=>		'Elektron manzil noto\'g\'ri kiritilgan',
+'err-pass' 					=>		'Parolda kirillcha harflar qo\'llanilgan',
+'no-account' 				=>		'Akkaunt topilmadi',
+'continue' 					=>		'Davom etish',
+'about-us' 					=>		'Biz haqimizda',
+'rules' 					=>		'Qoidalar',
+'faq' 						=>		'Ko\'p so\'raladigan savollar',
+'contacts' 					=>		'Aloqa',
+'admins' 					=>		'Adminlar',
+'no-money' 					=>		'Pul yetarli emas',
+'page' 						=>		'Sahifa',
+'custom' 					=>		'Maxsus',
+'official' 					=>		'Rasmiy',
+'level'    					=>		'Status',
+'user' 						=>		'Foydalanuvchi',
+'moder' 					=>		'Moderator',
+'admin' 					=>		'Adminstrator',
+'founder' 					=>		'Osiyo',
+'show-comm' 				=>		'Kommentarlarni ko\'rsatish',
+'share' 					=>		'Ulashish',
+'attach' 					=>		'Yuklash',
+'upload' 					=>		'Yuklab olish',
+'upload-text' 				=>		'Mumkin bo\'lgan fayl formatlari: png, jpg, jpeg, gif, mp4 va mov.
+Video fayllari 3 daqiqaga qadar davomiyligi va hajmi 4MB dan ko\'p bo\'lmagan bo\'lishi kerak',
+'upload-file' 				=>		'Rasm va video bir vaqtda yuklab olinmaydi',
+'upload-type' 				=>		'Qo\'llab-quvvatlanmaydigan format',
+'upload-size' 				=>		'Fayl juda katta',
+'notify-comm'    			=>		'sizning nashringizga izoh qo\'ydi',
+'notify-like'    			=>		'sizning nashringiz yoqdi',
+'notify-follow'    			=>		'sizni obunabdi',
+'notify-request'    		=>		'So\'rovni qo\'llash',
+'notify-request-confirm'    =>		'So\'rovlarni tasdiqlash yoki rad etish',
+'main'    					=>		'Umumiy',
+'contacts'    				=>		'Aloqa',
+'ban'    					=>		'Cheklash',
+'keywords'    				=>		'Mualliflik huquqi',
+'description'    			=>		'Tavsif',
+'access'    				=>		'Muqobil',
+'access-1'    				=>		'Hamma',
+'access-2'    				=>		'Faqat ro\'yxatdan o\'tganlar',
+'access-3'    				=>		'Faqat adminstratorlar',
+'reg-1'    					=>		'Uchun ochiq',
+'reg-2'    					=>		'Tasdiqlash bilan',
+'reg-3'    					=>		'Yopiq',
+'publics'    				=>		'Nashrlar',
+'elem'    					=>		'Nashrlar',
+'official-page'    			=>		'Rasmiy sahifa',
+'video-time'    			=>		'Video vaqti',
+'video-size'    			=>		'Video hajmi',
+'users'    					=>		'Foydalanuvchilar',
+'blocked'    				=>		'Cheklangan',
+'visits'    				=>		'Tashriflar',
+'cause'    					=>		'Sabab',
+'time'    					=>		'Vaqt',
+'block'    					=>		'Cheklash',
+'unlock'    				=>		'Cheklashni o\'chirish',
+'ac-block'    				=>		'Akkaunt cheklangan',
+'ac-unlock'    				=>		'Akkaunt o\'chirildi',
+'ac-block-time'    			=>		'Akkauntni hohlasa qo\'yib qo\'yish mumkin',
+'unmute'    				=>		'Ovozni yoqish uchun bosing',
+'keyboard'    				=>		'Keyingi koʻrish uchun boʻsh joy tugmasini bosing',
+'day1' 		    			=>		'Bugun',
+'day2' 		    			=>		'Kecha',
+'day3' 		    			=>		'Kunduz',
+'jan' 		    			=>		'Yan',
+'feb' 		    			=>		'Fev',
+'mar' 		    			=>		'Mar',
+'apr' 		    			=>		'Apr',
+'may' 		    			=>		'May',
+'jun' 		    			=>		'Iyun',
+'jul' 		    			=>		'Iyul',
+'aug' 		    			=>		'Avg',
+'sen' 		    			=>		'Sen',
+'oct' 		    			=>		'Okt',
+'noy' 		    			=>		'Noy',
+'dec' 		    			=>		'Dek',
+'ago'          				=>   	'avval',
+'hours'        				=>   	'soat',
+'minute'       				=>   	'daqiqa',
+'minutes'      				=>   	'daqiqa',
+'fromnow'      				=>   	'hozirgi',
+'seconds'      				=>   	'soniya',
+'yesterday'    				=>   	'kecha',
+'tomorrow'     				=>   	'ertaga',
+'days'         				=>   	'kun',
+'hour' 		    			=>		'Soat',
+'day' 		    			=>		'Kun',
+'week' 		    			=>		'Hafta',
+'month' 		    		=>		'Oy',
+'months' 		    		=>		'Oylar',
+'months2' 		    		=>		'Oy',
+'year' 		    			=>		'Yil',
+'january' 		    		=>		'Yanvar',
+'february' 		    		=>		'Fevral',
+'march' 		    		=>		'Mart',
+'april' 		    		=>		'April',
+'may' 		    			=>		'May',
+'june' 		    			=>		'Iyun',
+'july' 		    			=>		'Iyul',
+'august' 		    		=>		'Avgust',
+'september' 				=>		'Sentyabr',
+'october' 		    		=>		'Oktyabr',
+'november' 		    		=>		'Noyabr',
+'december' 		    		=>		'Dekabr',
+'about-us-text' => '
+
+<div class="mb-10">
+<b>Wondergam</b> ijtimoiy tarmoq!
+</div>
+<div class="mb-10">
+Wondergam - bu virtual jamiyat, jahon siz suhbatlashishingiz, qiziq odamlarni tanishingiz, suratlarni va videoroliklarni ulashingiz, o\'zingizning fikringizni ifodalashingiz, hayotingizning muhim paytlarini saqlashingiz, shaxsiy chatlarda suhbatlashishingiz va hayotingizning jazibali paytlarini ulashingiz mumkin.
+</div>
+Wondergam - sizning ijtimoiy muloqotlaringiz uchun cheklanmas yo\'llar manzili.
+
+',
+'sogl-text' => '
+
+<div class="mb-10">
+Shartlar bilan tan olish va rozi bo\'lish. Ijtimoiy tarmoqqa ro\'yxatdan o\'tgan va uni foydalanuvchi shu Shartnomaga avtomatik ravishda rozi bo\'lishadi.
+</div>
+<div class="mb-10">
+Foydalanuvchi mas\'uliyati. Foydalanuvchi ijtimoiy tarmoqda chop etgan ma\'lumotlarining, shu jumladan mualliflik huquq va ijtimoiy tarmoq qonunlariga va qoidalariga muvofiqligi to\'g\'risida to\'liq javobgarlikni o\'z zimmasiga oladi.
+</div>
+<div class="mb-10">
+Boshqa foydalanuvchilar bilan hamkorlik. Foydalanuvchi boshqa foydalanuvchilarning huquqlarini hurmat qilish, ularni iftixor etmamaslik va ijtimoiy tarmoq qonunlarini buzmaslik sharti bilan to\'g\'ri kelishadi.
+</div>
+<div class="mb-10">
+Shaxsiy ma\'lumotlarning maxfiyati. Ijtimoiy tarmoq foydalanuvchilarining shaxsiy ma\'lumotlarini maxfiy saqlash va ularni foydalanuvchi rozi bo\'lmaganicha uchraydi, faqat tashkilotni qonuniga o\'xshash holatlarda, u boshqa shaxslarga o\'tkazilmaydi.
+</div>
+<div class="mb-10">
+O\'chirish huquqi. Foydalanuvchiga o\'z ma\'lumotlarini ijtimoiy tarmoqdan istalgan holatda o\'chirish huquqi bor.
+</div>
+<div class="mb-10">
+Ijtimoiy tarmoq mas\'uliyati. Ijtimoiy tarmoq foydalanuvchilariga yoki uni foydalanuvchilari bilan bog\'liq bo\'lmagan, uchun tashqariga zarar yetkazadigan har qanday zararlar uchun javobgar emas.
+</div>
+<div class="mb-10">
+Shartnomani o\'zgartirish. Ijtimoiy tarmoq shartlarini o\'z rejamda va foydalanuvchilarga xabar qilmasdan o\'zgartirish huquqiga ega bo\'lishi mumkin. Foydalanuvchi shartnomani o\'zgartirishlarni bilish uchun uning shartlarini doimiy tekshirish zarur.
+</div>
+',
+'rules-text' => '
+
+<div class="mb-10">
+Foydalanuvchilar munosabatlari. Foydalanuvchilar ijtimoiy tarmoqda mas\'ulona davranishlari va boshqa foydalanuvchilarni hurmat qilishlari kerak. Qarashli, diskriminatsiya, nomaqbul yoki pornografiya materiallarini, shuningdek inson huquqlarini buzadigan matnlarini, zulm va boshqa hujjat buzishlarni chop etish mana. 
+</div>
+<div class="mb-10">
+Shaxsiy ma\'lumotlarni maxfiy saqlash. Foydalanuvchilar o\'z shaxsiy ma\'lumotlari maxfiylik qoidalari asosida harakat qilishlari, o\'z ro\'yxat va parolni boshqalarga o\'tkazmasliklari kerak. Foydalanuvchilar boshqalarning rozili bo\'lmaganicha ma\'lumotlarini yig\'ish, saqlash yoki foydalanishlarini taqiqlanadi.
+</div>
+<div class="mb-10">
+Avtorlik huquqi. Foydalanuvchilar avtorlik huquqqa amal qilishlari, o\'z ruxsatini olmagan holda boshqalar to\'g\'risidagi materiallarni chop etmasliklari kerak. Foydalanuvchilar matnlarini o\'z egasi ruxsat bermaganicha avtorlik huquqqa ega bo\'lmagan materiallarni ishlatmasliklari kerak.
+</div>
+<div class="mb-10">
+Reklama va spamlash. Ijtimoiy tarmoqda, to\'sqin reklama yoki spamni chop etish qoralangan. Tijorat reklama faqat maqbul shartlar va ijtimoiy tarmoq administratorlarining roziligi bilan mumkin.
+</div>
+<div class="mb-10">
+Mavjud emas materiallar. Ijtimoiy tarmoqda, viruslar, zararli dasturlar yoki boshqa shaxsni zarar yetkazishi mumkin bo\'lgan elementlar saqlash shart. Boshqalar yoki ijtimoiy tarmoqga zarar yetkazish mumkin bo\'lgan materiallarni chop etmaslik kerak.
+</div>
+<div class="mb-10">
+Administratorlar bilan hamkorlik. Foydalanuvchilar ijtimoiy tarmoqning qoidalarini buzishni ko\'rgan har qanday tashqi huquqni administratorlarga xabar qilishlari, ular bilan ishlashlari kerak.
+</div>
+',
+
+'contacts-text' 	=>		'Savollaringiz bo\'lsa, ikkilanmasdan loyiha ma\'muriyati bilan quyidagi kontaktlar orqali bog\'lanishingiz mumkin: pochta - Xaotik1998@gmail.com yoki Telegram - @xaori69otori. Biz har doim sizga yordam berishga tayyormiz!',
+
+'download-text' 	=>		'Android uchun Wondergram ijtimoiy tarmoq ilovasi!',
+
+);
+
+?>
